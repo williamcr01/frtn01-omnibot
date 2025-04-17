5eedd278d0674c4d2ebb33c2b91a98723c563059
William Crona <williamcrona@gmail.com>
2025-04-17 10:10:38 +0200

    disperser

--- HCA/disperser.py
+++ HCA/disperser.py
@@ -1,7 +1,18 @@
 import numpy as np
-import threading
 
-class Disperser(threading.Thread):
-    
-    def __init__(self):
-        super().__init__()
+def harmonic_array(x, t, T, H):
+    omega = 2 * np.pi / T
+    X_harmonic = np.zeros((H + 1, len(t)), dtype=complex)
+
+    for k in range(len(t)):
+        tk = t[k]
+        idx = (t >= (tk - T)) & (t <= tk)
+        tau = t[idx]
+        x_segment = x[idx]
+
+        for h in range(H + 1):
+            exponential = np.exp(-1j * h * omega * tau)
+            integral = np.trapz(x_segment * exponential, tau)
+            X_harmonic[h, k] = (1 / T) * integral
+
+    return X_harmonic
