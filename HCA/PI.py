129e5588d2e2a037434a585424d36fefe67e3fa2
William Crona <williamcrona@gmail.com>
2025-04-18 10:43:11 +0200

    PI funkar

--- HCA/PI.py
+++ HCA/PI.py
@@ -7,18 +7,18 @@ class PI():
         self.v = 0.0
         self.e = 0.0
         self.H = H
-        self.p = PIParameters.PIParameters(K=1.0, Ti=0.1, Tr=10.0, Beta=1.0, H=self.H, integrator_on=True)
+        self.p = PIParameters.PIParameters(K=5.0, Ti=12.0, Tr=36.0, Beta=1.0, H=self.H, integrator_on=True)
 
     def calculate_output(self, y, yref):
-        self.e = yref - y;
-        self.v = self.p.K * (self.p.Beta * yref - y) + self.I;
+        self.e = yref - y
+        self.v = self.p.K * (self.p.Beta * yref - y) + self.I
         return self.v;
 
     def update_state(self, u):
         if self.p.integrator_on:
-            self.I = self.I + (self.p.K * self.p.H / self.p.Ti) * self.e + (self.p.H / self.p.Tr) * (u - self.v);
+            self.I = self.I + (self.p.K * self.p.H / self.p.Ti) * self.e + (self.p.H / self.p.Tr) * (u - self.v)
         else:
-            self.I = 0.0;
+            self.I = 0.0
     
     def get_H_millis(self):
         return self.p.H * 1000
--- HCA/regul.py
+++ HCA/regul.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import PI
 import threading
+import PIParameters
 
 class Regul(threading.Thread):
 
@@ -12,6 +13,8 @@ class Regul(threading.Thread):
         self.PI_x = PI.PI(self.H)
         self.PI_y = PI.PI(self.H)
         self.PI_theta = PI.PI(self.H)
+        self.param_theta = PIParameters.PIParameters(K=4.0, Ti=0, Tr=10.0, Beta=1.0, H=self.H, integrator_on=False)
+        self.set_PI_param(self.PI_theta, self.param_theta)
         self.refgen = None
         self.should_run = True
         # Robot Parameters
@@ -21,8 +24,8 @@ class Regul(threading.Thread):
     def set_refgen(self, refgen):
         self.refgen = refgen
 
-    def set_PI_param(self, params):
-        self.PI.set_parameter(params)
+    def set_PI_param(self, PI, params):
+        PI.set_parameters(params)
     
     def get_PI_param(self, pi):
         return pi.p
@@ -30,7 +33,7 @@ class Regul(threading.Thread):
     def shutdown(self):
         self.should_run = False
 
-    def limit(self, v, min=-10, max=10): # ändra dess till riktiga värden
+    def limit(self, v, min=-1000, max=1000): # ändra dess till riktiga värden
         if v < min:
             v = min
         elif v > max:
@@ -49,21 +52,40 @@ class Regul(threading.Thread):
         return wheel_speeds
     
     def run(self):
+        #x_dot, y_dot, theta_dot = 0
+        x_old = self.bot.get_x()
+        y_old = self.bot.get_y()
         print("regul thread started")
         while self.should_run:
             print("regul thread running")
             theta = self.bot.get_theta()
             theta = theta * np.pi / 180
+            x = self.bot.get_x()
+            y = self.bot.get_y()
             x_ref, y_ref = self.refgen.get_ref()
-            x_dot = self.PI_x.calculate_output(self.bot.get_x(), x_ref)
-            y_dot = self.PI_y.calculate_output(self.bot.get_y(), y_ref)
+            x_dot = self.PI_x.calculate_output(x, x_ref)
+            y_dot = self.PI_y.calculate_output(y, y_ref)
             theta_dot = self.PI_theta.calculate_output(theta, 0)
-            self.PI_x.update_state(x_dot)
-            self.PI_y.update_state(y_dot)
-            self.PI_theta.update_state(theta_dot)
+            if abs(x_dot) < 0.5:
+                self.PI_x.update_state(x_dot)
+            else:
+                self.PI_x.update_state(x-x_old)
+            if abs(y_dot) < 0.5:
+                self.PI_y.update_state(y_dot)
+            else:
+                self.PI_y.update_state(y-y_old)
+            """ if abs(theta_dot) < 0.1:
+                self.PI_theta.update_state(theta_dot)
+            else:
+                self.PI_theta.update_state(0.1) """
 
             wheel_speeds = self.inverse_kinematics(theta, x_dot, y_dot, theta_dot)
+            for i, v in enumerate(wheel_speeds):
+                wheel_speeds[i] = self.limit(v)
             self.write_output(wheel_speeds)
+
+            x_old = x
+            y_old = y
 
             time.sleep(self.H)
             
